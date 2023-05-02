1890732fdad4d8b1efb79c30b18dda0608450fcd
luisf <luisfelipecf94@gmail.com>
2023-05-02 11:18:51 -0300

    moving Examples component to different file

---
+++ features/translation/src/main/java/com/luisfagundes/translation/components/Examples.kt
@@ -0,0 +1,28 @@
+package com.luisfagundes.translation.components
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import com.luisfagundes.domain.models.Example
+import com.luisfagundes.theme.spacing
+
+@Composable
+fun Examples(examples: List<Example>) {
+    Spacer(
+        modifier = Modifier.padding(vertical = MaterialTheme.spacing.verySmall)
+    )
+    examples.forEach { example ->
+        Text(text = example.destinationLanguage)
+        Spacer(
+            modifier = Modifier.padding(vertical = MaterialTheme.spacing.extraSmall)
+        )
+        Text(
+            text = "\"${example.sourceLanguage}\"",
+            fontStyle = FontStyle.Italic
+        )
+    }
+}
