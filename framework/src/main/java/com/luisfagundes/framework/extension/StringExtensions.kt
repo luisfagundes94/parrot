22622945aac7ff039a235722064952b4fca0e5ee
luisf <luisfelipecf94@gmail.com>
2023-05-02 11:18:33 -0300

    adding capitalize string extension

--- framework/src/main/java/com/luisfagundes/framework/extension/StringExtensions.kt
+++ framework/src/main/java/com/luisfagundes/framework/extension/StringExtensions.kt
@@ -1,5 +1,7 @@
 package com.luisfagundes.framework.extension
 
+import java.util.Locale
+
 private const val EMPTY_STRING = ""
 
 fun List<String>.convertListToString() = joinToString(", ")
@@ -7,3 +9,7 @@ fun List<String>.convertListToString() = joinToString(", ")
 fun String.Companion.empty() = EMPTY_STRING
 
 fun String.formatDate() = split("-").reversed().joinToString("/")
+
+fun String.capitalize() = replaceFirstChar {
+    if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
+}
