647e379b2898fc3fec7a4dce3c000d4285fcf67e
luisf <luisfelipecf94@gmail.com>
2023-05-06 12:35:01 -0300

    renaming definitions of to examples of

--- features/translation/src/main/java/com/luisfagundes/languages/LanguageListScreen.kt
+++ features/translation/src/main/java/com/luisfagundes/languages/LanguageListScreen.kt
@@ -1,14 +1,11 @@
 package com.luisfagundes.languages
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -20,21 +17,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.luisfagundes.commons_ui.ParrotTopBar
 import com.luisfagundes.domain.models.Language
+import com.luisfagundes.framework.components.ErrorView
 import com.luisfagundes.framework.components.LoadingView
 import com.luisfagundes.theme.ParrotLingoTheme
-import com.luisfagundes.theme.md_theme_dark_card
-import com.luisfagundes.theme.md_theme_light_card
 import com.luisfagundes.theme.spacing
 import com.luisfagundes.translation.R
 
@@ -50,6 +41,10 @@ fun LanguageListScreen(
 
     when {
         uiState.isLoading -> LoadingView()
+        uiState.countries.isEmpty() -> ErrorView(
+            message = "No results were found!",
+            animationId = R.raw.warning
+        )
         uiState.countries.isNotEmpty() -> LanguageList(
             countries = uiState.countries,
             onBackPressed = onBackPressed
