a5f8d50b4a6e0625d8297bb7d4ffa950a033a468
luisf <luisfelipecf94@gmail.com>
2023-05-10 18:05:19 -0300

    updating deprecated .textFieldColors to .colors

--- features/translation/src/main/java/com/luisfagundes/languages/components/LanguageSearch.kt
+++ features/translation/src/main/java/com/luisfagundes/languages/components/LanguageSearch.kt
@@ -3,19 +3,17 @@ package com.luisfagundes.languages.components
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import com.luisfagundes.languages.LanguageListEvent
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LanguageSearch(
     modifier: Modifier = Modifier,
@@ -37,10 +35,11 @@ fun LanguageSearch(
         leadingIcon = {
             Icon(Icons.Filled.Search, contentDescription = null)
         },
-        colors = TextFieldDefaults.textFieldColors(
+        colors = TextFieldDefaults.colors(
             cursorColor = MaterialTheme.colorScheme.onSurface,
-            textColor = MaterialTheme.colorScheme.onSurface,
-            containerColor = MaterialTheme.colorScheme.surface
+            focusedTextColor = MaterialTheme.colorScheme.onSurface,
+            focusedContainerColor = MaterialTheme.colorScheme.surface,
+            unfocusedContainerColor = Color.Transparent,
         )
     )
 }
