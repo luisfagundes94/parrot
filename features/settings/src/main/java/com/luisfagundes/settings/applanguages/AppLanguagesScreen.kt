9907cd18ddd81520a89541c04386222e8c16c0f4
luisf <luisfelipecf94@gmail.com>
2023-05-20 14:32:59 -0300

    spotless scan

--- features/settings/src/main/java/com/luisfagundes/settings/applanguages/AppLanguagesScreen.kt
+++ features/settings/src/main/java/com/luisfagundes/settings/applanguages/AppLanguagesScreen.kt
@@ -3,8 +3,6 @@ package com.luisfagundes.settings.applanguages
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
